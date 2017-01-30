68d709ae9184b7f05c483d5ed1be69e50632e6f8
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-30 17:57:43 +0100

    init

---
+++ main.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+
+def dogeify(text)
+  articles   = ['the', 'a', 'an']
+  doge_words = ['many', 'such', 'wow', 'so']
+  colors = ['red', 'green', 'blue', 'yellow', 'pink', 'orange']
+
+  text.split ' '.map do |w|
+    if articles.include? w.downcase
+      "<span style='color: #{colors.sample}'>
+        #{doge_words.sample}
+       </span>"
+    else
+      w
+    end
+  end.join ' '
+end
+
+
+get "/" do
+  erb :index
+end
+
+post "/" do
+  @text = dogeify(params[:text])
+  erb :result
+end
